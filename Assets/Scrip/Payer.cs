f8bdb05d901e020d694cb61d5c7d06e11242f64c
Jhordan Garcia <jhordantm@gmail.com>
2022-08-29 16:28:19 -0500

    Arreglada

--- Assets/Scrip/Payer.cs
+++ Assets/Scrip/Payer.cs
@@ -4,73 +4,105 @@ using UnityEngine;
 
 public class Payer : MonoBehaviour
 {
-    public float velocity = 10,jumpForce = 5 ;
+    public float JumpForce = 5;
+    public float velocity = 10;
+    public float vcorrer = 20;
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator animator;
-    const int ANIMACION_QUIETO=0;
-    const int ANIMACION_CAMINAR=1;
-    const int ANIMACION_CORRER=2;
-    const int ANIMACION_ATACAR =3;
-    const int ANIMACION_SALTAR =4;
-     bool puedaSaltar = true;
+    const int ANIMATION_QUIETO = 0;
+    const int ANIMATION_CAMINAR = 1;
+    const int ANIMATION_CORRER = 2;
+    const int ANIMATION_ATACAR = 3;
+    const int ANIMATION_Saltar = 4;
+    bool puedeSaltar = true;
     // Start is called before the first frame update
     void Start()
     {
-       
-        Debug.Log("Este es un mensaje de log");
-
+    
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-       
-          
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-        rb.velocity = new Vector2(0,rb.velocity.y);
-        cambioanimacion(ANIMACION_QUIETO);
-
-        if(Input.GetKey(KeyCode.RightArrow)){
-            rb.velocity = new Vector2(velocity,rb.velocity.y);
-            sr.flipX=false;
-            cambioanimacion(ANIMACION_CORRER);   
-        }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            rb.velocity = new Vector2(-velocity,rb.velocity.y);
-            sr.flipX=true;  
-            cambioanimacion(ANIMACION_CAMINAR);
-         }
-        else if(Input.GetKey(KeyCode.X)){
-            rb.velocity = new Vector2(velocity,rb.velocity.y);
-            sr.flipX=true;  
-            cambioanimacion(ANIMACION_CORRER);
-         }
-
-        if(Input.GetKey(KeyCode.Z)){ 
-            cambioanimacion(ANIMACION_ATACAR);
-         }
-
-        if (Input.GetKeyUp(KeyCode.Space)  && puedaSaltar)
+       
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.X))
         {
-            rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
-            puedaSaltar =  false;
-            cambioanimacion(ANIMACION_SALTAR);
+            rb.velocity = new Vector2(-vcorrer, rb.velocity.y);
+            sr.flipX = true;
+            ChangeAnimation(ANIMATION_CORRER);
         }
-        
+
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector2(vcorrer, rb.velocity.y);
+            sr.flipX = false;
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(velocity, rb.velocity.y);
+            sr.flipX = false;
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocity, rb.velocity.y);
+            sr.flipX = true;
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            ChangeAnimation(ANIMATION_ATACAR);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && puedeSaltar == true)
+        {
+            
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            ChangeAnimation(ANIMATION_Saltar);
+            puedeSaltar = false;
+
+           
+
+
+
+        }
+   
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            ChangeAnimation(ANIMATION_QUIETO);
+        }
     }
+
+
     void OnCollisionEnter2D(Collision2D other)
-     {  // saber cuando colsiona
-        puedaSaltar = true;
-      
+    {
+        puedeSaltar = true;
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Estas muerto");
+        }
     }
 
- 
 
-    private void cambioanimacion(int animation){ // animaciones de caminar y quieto
+    private void ChangeAnimation(int animation)
+    {
         animator.SetInteger("Estado0", animation);
+       
+
     }
+    private void cambioAnimation(bool anima)
+    {
+        animator.SetBool("puedeSaltar", anima);
+
+
+    }
+
 }
